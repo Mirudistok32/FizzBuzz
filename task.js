a84d5935d967f05ff429c756bf4a274e45d02ab4
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-12 20:24:42 +0300

    Refactor

--- task.js
+++ task.js
@@ -3,13 +3,11 @@ for (let i = 0; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0 && i != 0) {
         console.log("FizzBuz")
         continue
+    } else if (i % 3 === 0  && i != 0) {
+        console.log("Fizz")
+    } else if (i % 5 === 0 && i != 0) {
+        console.log("Buzz")
     } else {
-        if (i % 3 === 0 && i != 0) {
-            console.log("Fizz")
-        }
-        if (i % 5 === 0 && i != 0) {
-            console.log("Buzz")
-        }
+        console.log(i)
     }
-    console.log(i)
 }
