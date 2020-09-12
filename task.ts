c858cc929603d103fab2d19c2fb99c300e95227d
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-12 20:39:02 +0300

    r

--- task.ts
+++ task.ts
@@ -5,8 +5,10 @@ for (let i = 0; i <= 100; i++) {
         continue
     } else if (i % 3 === 0  && i != 0) {
         console.log("Fizz")
+        continue
     } else if (i % 5 === 0 && i != 0) {
         console.log("Buzz")
+        continue
     } else {
         console.log(i)
     }
